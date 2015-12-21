eef190f748853614a8c2bb8c2f2c974a35a90d84
Alex Macleod <alex@macleod.io>
2015-12-21 14:02:03 +0000

    Day 21

--- 21/part-1.rb
+++ 21/part-1.rb
@@ -1,11 +1,8 @@
-boss   = IO.read('input').scan(/\d+/).map(&:to_i)
-shop = {
-  weapons: [[8, 4, 0],  [10, 5, 0], [25, 6, 0],  [40, 7, 0], [74, 8, 0]],
-  armor:   [[13, 0, 1], [31, 0, 2], [53, 0, 3],  [75, 0, 4], [102, 0, 5]],
-  rings:   [[25, 1, 0], [50, 2, 0], [100, 3, 0], [20, 0, 1], [40, 0, 2], [80, 0, 3]]
-}
-shop.each_key { |k| shop[k] << [0, 0, 0] unless k == :weapons }
-p shop[:rings].product(*shop.values).select { |e|
-  e[0] != e[-1] &&
+boss = IO.read('input').scan(/\d+/).map(&:to_i)
+shop = IO.read('shop').split(/\n\n/).map { |e| e.scan(/(?<!\+)\d+/).map(&:to_i).each_slice(3).to_a }
+
+shop.each_index { |i| shop[i] << [0, 0, 0] unless i.zero? }
+p shop[-1].product(*shop).select { |e|
+  (e[0] != e[-1] || e[0] == [0, 0, 0]) &&
     boss[0] / [e.reduce(0) { |a, e| a + e[1] } - boss[2], 1].max <= 100 / [boss[1] - e.reduce(0) { |a, e| a + e[2] }, 1].max
 }.min_by { |e| e.reduce(0) { |a, e| a + e[0] } }.reduce(0) { |a, e| a + e[0] }
--- 21/shop.rb
+++
@@ -1,1 +0,0 @@
-p IO.read('shop').split(/\n\n/).map { |e| e.scan(/\d+/).map(&:to_i).each_slice(3).to_a }
